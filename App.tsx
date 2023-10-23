8345dac62d868e53726efc159bbcbaaf1d4ea4ab
Adrian Herrera <adrian.herrera@salesianos-lacuesta.net>
2023-10-23 23:08:22 +0100

    feat: added tsx app structure

--- App.tsx
+++ App.tsx
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View, ScrollView } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 export default function App() {
+  const [displayMyInfo, setDisplayMyInfo] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.topContainer}>
+        <View style={styles.firsttoprowContainer}>My Portfolio App</View>
+        <View style={styles.rowTopSecondContainer}>
+          <Pressable onPress={() => setDisplayMyInfo(true)}>Mi info</Pressable>
+          <Pressable onPress={() => setDisplayMyInfo(false)}>Mi Repo</Pressable>
+        </View>
+      </View>
+      {
+        displayMyInfo ?
+          <View style={styles.bodystails}>
+            <Text>
+              Descripción sobre mí!
+            </Text>
+            <Text>
+              Cosas que me gustan:
+            </Text>
+            <ScrollView>
+              <Text>Cosa uno</Text>
+              <Text>Cosa dos</Text>
+              <Text>Cosa tres</Text>
+              <Text>Cosa cuatro</Text>
+              <Text>Cosa cinco</Text>
+              <Text>Cosa seis</Text>
+            </ScrollView>
+          </View>
+          :
+          <View style={styles.bodystails}>
+            <QRCode
+              value="https://github.com/adhernea"
+            />
+          </View>
+      }
     </View>
   );
 }
@@ -17,4 +49,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  topContainer: {},
+  firsttoprowContainer: {},
+  rowTopSecondContainer: {},
+  bodystails: {
+
+  }
 });
