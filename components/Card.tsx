d799e7cd029ddf1746baa5b0fa882096dc6067b4
adansif <adan.yanesgonzalez@gmail.com>
2023-10-30 12:10:13 +0000

    feat(LightModeButton): Added light/dark theme mode

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 
-type CardProps = {
+interface CardProps {
     text: string
 }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,46 @@
 import React from "react";
 import { Button, Pressable, Text, View, StyleSheet } from "react-native";
+import LightModeButton from "./LightModeButton";
 
 type HeaderProps = {
+  setIsLightMode: React.Dispatch<React.SetStateAction<boolean>>
+  isLightMode: boolean
   headerText : string
+
 }
 
-const Header: React.FC<HeaderProps> = ({headerText: headerText}) => {
+const Header: React.FC<HeaderProps> = ({
+  setIsLightMode: setIsLightMode,
+  isLightMode: isLightMode,
+  headerText: headerText
+}) => {
   return (
-    <View style={styles.topContainer}>
-      <Text style={styles.firstTopRowContainer}>{headerText}</Text>
-      
+    <View style={[styles.topContainer, (isLightMode? styles.light: styles.dark)]}>
+      <Text style={[styles.firstTopRowContainer, (isLightMode? {color: 'black'}: {color: 'white'})]}>{headerText}</Text>
+      <LightModeButton setIsLightMode={setIsLightMode} isLightMode={isLightMode}/>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   topContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
     paddingTop: 50,
     width: "100%",
   },
   firstTopRowContainer: {
-    backgroundColor: "#43D6F5",
     textAlign: "center",
     fontWeight: "bold",
     textAlignVertical: "center",
     fontSize: 30,
+  },
+  light: {
+    backgroundColor: "#43D6F5",
+  },
+  dark: {
+    backgroundColor: "#232D3F",
   },
 });
 
